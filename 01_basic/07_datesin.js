d12863587a908e070808143b3ba638a82e21d7f7
vikramkumar <vikram.mahto.it@gmail.com>
2026-06-13 13:28:47 +0000

    DateandTime

---
+++ 01_basic/07_datesin.js
@@ -0,0 +1,13 @@
+// let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+
+// let myCreatedDate = new Date(2026, 0, 13);
+ let myCreatedDate = new Date("2026-06-13");
+// console.log(myCreatedDate.toString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
