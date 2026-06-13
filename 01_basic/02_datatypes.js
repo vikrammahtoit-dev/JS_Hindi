422abb31abdda42237f7c92c2dbd3965e11740e2
vikramkumar <vikram.mahto.it@gmail.com>
2026-06-13 06:36:13 +0000

    add datatype file

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"
+
+console.log("Hello World!");
+
+let name = "Vikram Kumar";
+let age = 24;
+let isLoggedIn = true;
+
+
+//Type of data type 
+/* PRIMITIVE DATA TYPES 
+1. Number 
+2. String 
+3.boolean
+4. null => empty value 
+5. undefined  => variable is declared but not assigned any value 
+6. Symbol => unique identifier
+7.BigInt => used to represent largge integer values
+*/
+
+console.log(typeof undefined);
+console.log(typeof null);
