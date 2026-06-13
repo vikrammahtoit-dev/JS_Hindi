8d7c6ed3631b9519a584753f4fcb45634977f4e8
vikramkumar <vikram.mahto.it@gmail.com>
2026-06-13 06:39:52 +0000

    update 01_basic/01_variables.js

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -1,4 +1,13 @@
-name = "Vikram Kumar"
+
+let name = "Vikram Kumar"
 console.log(name);
 name = "Raj Kumar";
 console.log(name);
+
+/*
+let 
+const 
+var 
+
+prefer not to use var because of its function scope and global scope issues .
+*/
