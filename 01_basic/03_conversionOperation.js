f67799735bf7d2b1fbd262738cd3a4629d7ddb14
vikramkumar <vikram.mahto.it@gmail.com>
2026-06-13 10:48:47 +0000

    update Operation file

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,7 +1,32 @@
-let score = "33abc";
-console.log(typeof score);
-console.log(typeof score);
+// let score = "33abc";
+// console.log(typeof score);
+// console.log(typeof score);
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let value = 3
+// let negValue = -value;
+// console.log(negValue);
+
+// let str1 = "Hello";
+// let str2 = "Vikram";
+// let str3 = str1 + " " + str2;
+// console.log(str3);
+
+// console.log("1" + "2");
+// console.log("1" + 2);
+// console.log(1+ "2");
+// console.log("1" +2 + 2);
+// console.log(1+2+ "3");
+
+// console.log(true + true);
+// console.log(+ true);
+// console.log(+" ");
+
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+console.log(gameCounter);
