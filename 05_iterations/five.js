5ea2abae68f421f686c138a134775c8afe000a28
vikramkumar <vikram.mahto.it@gmail.com>
2026-06-14 15:43:32 +0000

    five

---
+++ 05_iterations/five.js
@@ -0,0 +1,48 @@
+const coding = ["js", "rb", "py", "c++", "java"];
+
+// coding.forEach( function (item) {
+//     console.log(item);
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach( printMe);
+
+const myCoding = [
+    {
+        languageName : "JavaScript",
+        languageFileName : "JS"
+    },
+
+    {
+        languageName : "JavaScript",
+        languageFileName : "js"
+    },
+
+    {
+        languageName : "Java",
+        languageFileName : "java"
+    },
+
+    {
+        languageName : "python",
+        languageFileName : "py"
+    },
+
+    {
+        languageName : "ruby",
+        languageFileName : "rb"
+    },
+
+    {
+        languageName : "C++",
+        languageFileName : "cpp"
+    },
+]
+
+myCoding.forEach( (item) =>{
+    console.log(item)
+
+})
