abcbd911cde54c18d4054246723b9dcad444b976
vikramkumar <vikram.mahto.it@gmail.com>
2026-06-14 15:43:14 +0000

    four

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+// for Object 
+
+// const myObject = {
+//     js: "JavaScript",
+//     cpp: "C++",
+//     rb : "rupby",
+//     swift : "Swift by apple"
+// }
+
+// for (const key in myObject) {
+//     // if (!Object.hasOwn(object, key)) continue;
+    
+//     // const element = object[key];
+
+//     // console.log(myObject[key]);
+
+//     console.log(`${key} is shortcut for ${myObject.value}`);  
+    
+// }
+
+const programming = ["JS", "Py", "rb", "Cpp", "Java"];
+for (const key in programming) {
+    // if (!Object.hasOwn(object, key)) continue;
+    
+    // const element = object[key];
+    console.log(programming[key]);
+     
+}
+
