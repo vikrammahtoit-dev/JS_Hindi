62af3979496a49e7cbac013620121ee43270f63a
vikramkumar <vikram.mahto.it@gmail.com>
2026-06-15 16:14:06 +0000

    BMI

---
+++ Project_two_BMI Generator/bmiCalculator.js
@@ -0,0 +1,48 @@
+const form = document.querySelector('form');
+// console.log(form);
+// this usecase will give empty value
+// const height= parseInt(document.querySelector("#height").value);
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+  const height= parseInt(document.querySelector("#height").value);
+  const weight = parseInt(document.querySelector("#weight").value);
+ const result = document.querySelector("#results");
+//   console.log(height);
+//   console.log(weight);
+if(height === '' || height <  0 || isNaN(height)){
+    results.innerHTML = `Please give a valid height ${height}`;
+}
+else if(weight === '' ||weight <  0 || isNaN(weight))
+{
+    result.innerHTML = `Please give a valid weightt ${weight}`;
+}else
+{
+    const bmi = (weight/((height*height)/1000).toFixed(2));
+
+    // show the result
+    result.innerHTML = `<span>${bmi}</span>`;
+}
+});
+
+// const form = document.querySelector('form');
+// // this usecase will give you empty
+// // const height = parseInt(document.querySelector('#height').value)
+
+// form.addEventListener('submit', function (e) {
+//   e.preventDefault();
+
+//   const height = parseInt(document.querySelector('#height').value);
+//   const weight = parseInt(document.querySelector('#weight').value);
+//   const results = document.querySelector('#results');
+
+//   if (height === '' || height < 0 || isNaN(height)) {
+//     results.innerHTML = `Please give a valid height ${height}`;
+//   } else if (weight === '' || weight < 0 || isNaN(weight)) {
+//     results.innerHTML = `Please give a valid weight ${weight}`;
+//   } else {
+//     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+//     //show the result
+//     results.innerHTML = `<span>${bmi}</span>`;
+//   }
+// });
+
